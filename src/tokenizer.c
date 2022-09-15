ff10fb561a35aa13b0271f3bd2effe81931002d3
FxrMillan <fxrmillan13@outlook.com>
2022-09-15 00:42:03 -0600

    made makefile 3 functions done

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+int space_char(char c){
+  return c == ' ' ? 1:0;
+}
+
+int non_space_char(char c){
+  return c != ' ' && c != '\0' ? 1:0;
+}
+
+char *word_start(char *str){
+  
+  char *tempStr = str;
+   while(non_space_char(*str)){
+    str += 1;
+  }
+   
+  if(space_char(*str)){
+      return str+=1;
+  }
+  else if(*str == '\0')
+  {
+      return tempStr;
+  }
+    return 0;
+}
 
--- src/userUi.c
+++ src/userUi.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include "history.h"
+#include "tokenizer.h"
+int main(){
 
+  puts("test Monkey");
+ int test = space_char('c');
+ char temp;
+ while (temp != '\n'){
+   scanf("%c",&temp);
+   test = non_space_char(temp);
+   printf("%d\n",test);
+ }
+ puts("done");
+
+ char tempString[] = "temp string\0";
+ char *ptr = word_start(tempString);
+ 
+ while (*ptr != '\0'){
+   printf("%c\n",*ptr);
+   ptr += 1;
+ }
+ 
+ }
