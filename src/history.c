c76f427ceb1d924fcc23403ecc2ea019d7ef49b0
Fxrmillan <fxrmillan13@outlook.com>
2022-09-24 10:55:23 -0600

    finished linked list

--- src/history.c
+++ src/history.c
@@ -1,1 +1,65 @@
+#include "history.h"
+#include <stdio.h>
+#include <stdlib.h>
+List* init_history(){
+  Item *HeadData = malloc(sizeof(Item));
+  List *headPtr = malloc(sizeof(List));
+  char *headStr = malloc(sizeof(char));
+  HeadData->id = 0;
+  HeadData->str= headStr;
+  HeadData->next = NULL;
+  headPtr ->root = HeadData;
+  printf("%s",headPtr->root->str);
+  return headPtr;
+}
+
+void add_history(List *list, char *str){
+  Item *listPtr = list->root;
+  while(listPtr->next != NULL){
+    listPtr = listPtr->next;
+  }
+  Item *tempData = malloc(sizeof(Item));
+  tempData ->id = listPtr->id + 1;
+  // printf("%d",tempData -> id);
+  tempData->str = str;
+  // printf("%s",tempData->str);
+  tempData->next = NULL;
+  listPtr->next = tempData;
+  // printf("%s",listPtr->next->str);
+}
+
+char *get_history(List *list, int id){
+  Item *listPtr = list->root;
+  while(listPtr != NULL){
+    if(listPtr->id == id){
+      return listPtr ->str;
+    }
+    listPtr = listPtr->next;
+  }
+
+}
+
+void print_history(List *list){
+  Item *listPtr = list->root->next;
+  while(listPtr!= NULL){
+    printf("%s",listPtr->str);
+    listPtr = listPtr ->next;
+  }
+}
+
+void free_history(List *list){
+  Item *listPtr = list->root;
+  Item *temp;
+  while(listPtr != NULL){
+    temp = listPtr;
+    free(temp->str);
+    listPtr = listPtr->next;
+    free(temp);
+  }
+  free(list);
+
+}
+
+
+
 
--- src/userUi.c
+++ src/userUi.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "history.h"
 #include "tokenizer.h"
 int main(){
 
- puts("test Monkey");
- char tempString[55]; 
- do{
+ char *tempString;
+ List *head = init_history();
+ // add_history(head,"DAVE\n");
+ // add_history(head,"STEVE\n");
+ // char*temp = get_history(head,3);
+ // printf("%s",temp);
+ // print_history(head);
+
+ 
+ 
+do {
    puts(">");
+   tempString = malloc(sizeof(char)*40);
    fgets(tempString, 50, stdin);
+   add_history(head,tempString);
    char **tokens = tokenize(tempString);
    print_tokens(tokens);
    free_tokens(tokens); 
-   }while(*tempString != '!');
+ } while(*tempString != '!');
+ print_history(head);
+ free_history(head);
+
+ 
  }
