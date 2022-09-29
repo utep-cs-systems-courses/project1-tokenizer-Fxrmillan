caf96c14fa8ead414a1ea837b9d67e3dcfe7e8e7
Fxrmillan <fxrmillan13@outlook.com>
2022-09-29 15:31:35 -0600

    user Ui complete

--- src/userUi.c
+++ src/userUi.c
@@ -6,38 +6,26 @@ int main(){
 
  char *tempString;
  List *head = init_history();
- int numberInp;
- // add_history(head,"DAVE\n");
- // add_history(head,"STEVE\n");
- // char*temp = get_history(head,3);
- // printf("%s",temp);
- // print_history(head);
-
- 
- 
+ unsigned int numberInp;
 do {
-   puts(">");
+   puts("> press Q to exit h for history and ! for specific history");
    tempString = malloc(sizeof(char)*40);
    fgets(tempString, 50, stdin);
-   if(*tempString != '!'){
+  if(*tempString == 'h' && tempString[1] == '\n' ){
+     printf("PRINTING HISTORY\n");
+     print_history(head);
+   }
+   else if(*tempString == '!'){
+     printf("PRINTING SELECTED STRING\n");
+     numberInp =(tempString[1] - '0');
+     printf("%s\n", get_history(head,numberInp));
+   }
+   else if(*tempString != 'Q'){
    add_history(head,tempString);
    char **tokens = tokenize(tempString);
    print_tokens(tokens);
    free_tokens(tokens);
    }
- } while(*tempString != '!');
- print_history(head);
- // free_history(head);
- do{
-   puts("> search history press -1 to exit");
-   scanf("%d",&numberInp);
-   if(numberInp == -1){
-     break;
-   }
-   char *temp = get_history(head,numberInp);
-   printf("%s\n",temp);
-
-
- }while(1);
+   } while(*tempString != 'Q');
  free_history(head);
  }
